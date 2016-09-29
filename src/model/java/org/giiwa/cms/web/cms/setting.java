24adcc901e36f4a344ee3a3802af1c510a7173ae
giiwa <yjiang@giisoo.com>
2016-09-29 12:52:14 +0800

    init

--- src/model/java/org/giiwa/cms/web/cms/setting.java
+++ src/model/java/org/giiwa/cms/web/cms/setting.java
@@ -38,4 +38,17 @@ public class setting extends Model {
     this.show("/cms/setting/category.html");
   }
 
+  @Path(path = "splash", login = true)
+  public void splash() {
+    SettingHelper h = new SettingHelper(login.getId());
+    if (method.isPost()) {
+      String splash = this.getString("splash");
+      h.set("splash", splash);
+    }
+
+    this.set("cates", Category.load(W.create("uid", login.getId()).sort("seq", 1), 0, 100));
+    this.set("helper", h);
+    this.show("/cms/setting/base.html");
+  }
+
 }
