e1a34eb1bcf93d159f76ee5a8ed5cf3e442e0543
giiwa <yjiang@giisoo.com>
2016-09-29 10:58:48 +0800

    update

--- src/model/java/org/giiwa/cms/bean/SettingHelper.java
+++ src/model/java/org/giiwa/cms/bean/SettingHelper.java
@@ -1,20 +1,31 @@
 package org.giiwa.cms.bean;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.giiwa.core.bean.Helper.W;
 
-public class PeopleHelper {
+public class SettingHelper {
 
   long uid;
 
-  public PeopleHelper(long uid) {
+  public SettingHelper(long uid) {
     this.uid = uid;
   }
 
   public String get(String name) {
-    People p = People.load(W.create("uid", uid).and("name", name));
+
+    Setting p = cache.get(name);
+    if (p == null) {
+      p = Setting.load(W.create("uid", uid).and("name", name));
+      cache.put(name, p);
+    }
     if (p != null) {
       return p.val;
     }
     return name;
   }
+
+  Map<String, Setting> cache = new HashMap<String, Setting>();
+
 }
